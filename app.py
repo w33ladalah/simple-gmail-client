2fe7a4f30342f83f5f7cee39403afc8d2ea88921
Hendro Wibowo <hendro@wonderlabs.io>
2017-04-01 18:18:39 +0700

    Add POST method

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from lib.gmail import read_email_from_gmail
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello World!"
+@app.route("/", methods=['GET', 'POST'])
+def main():
+    mails = []
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        folder = request.form['folder'] if request.form['folder'] else 'Inbox'
+        pattern = request.form['pattern'] if request.form['pattern'] else ''
+        mails = read_mails(email, password, folder, pattern)
+
+    return render_template('template.html', mails=mails)
+
+def read_mails(email, password, folder, pattern):
+    return read_email_from_gmail(email, password, folder, pattern)
 
 if __name__ == "__main__":
     app.run()
