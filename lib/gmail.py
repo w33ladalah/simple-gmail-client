65a87f2a30359b733f544acd4919f934f4b06f0c
Hendro Wibowo <hendro@wonderlabs.io>
2017-04-01 13:56:36 +0700

    Add html cleaner and filter function

--- lib/gmail.py
+++ lib/gmail.py
@@ -1,5 +1,7 @@
 import imaplib
 import email
+import re
+from bs4 import BeautifulSoup
 
 from config import SMTP_SERVER
 
@@ -9,7 +11,7 @@ from config import SMTP_SERVER
 #
 # ------------------------------------------------
 
-def read_email_from_gmail(email_address, email_password, folder='inbox'):
+def read_email_from_gmail(email_address, email_password, folder='inbox', extract_pattern='/(72 jam)/'):
     try:
         mail = imaplib.IMAP4_SSL(SMTP_SERVER)
         mail.login(email_address, email_password)
@@ -22,19 +24,30 @@ def read_email_from_gmail(email_address, email_password, folder='inbox'):
         first_email_id = int(id_list[0])
         latest_email_id = int(id_list[-1])
 
-
         for i in range(latest_email_id,first_email_id, -1):
             typ, data = mail.fetch(i, '(RFC822)' )
 
             for response_part in data:
                 if isinstance(response_part, tuple):
                     msg = email.message_from_string(response_part[1])
-                    email_subject = msg['subject']
-                    email_from = msg['from']
-                    email_body = msg['body']
-                    print 'From : ' + email_from + '\n'
-                    print 'Subject : ' + email_subject + '\n'
-                    print email_body
+                    body = clean_mail_body(msg)
+                    filtered_body = filter_text(body, extract_pattern)
+                    print filtered_body + "\n"
 
     except Exception, e:
         print str(e)
+
+def clean_mail_body(text):
+    input_html = str(text)
+    soup = BeautifulSoup(input_html, "html.parser")
+    output_text = soup.get_text()
+    return output_text
+
+def filter_text(text, pattern):
+    try:
+        p = re.compile(pattern)
+        found = p.search(text).group(1)
+    except AttributeError:
+        found = ''
+
+    return found
