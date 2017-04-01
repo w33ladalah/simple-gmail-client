d3cae1221359d7a1fc9721d52421a6e12df59ee6
Hendro Wibowo <hendro@wonderlabs.io>
2017-04-01 18:17:30 +0700

    Add command line paramater limit

--- cmd.py
+++ cmd.py
@@ -5,25 +5,35 @@ import sys, getopt
 from lib.gmail import read_email_from_gmail
 
 if __name__ == '__main__':
-    argv = sys.argv[1:]
-    email = ''
-    password = ''
-    folder = 'inbox'
     try:
-        opts, args = getopt.getopt(argv, "e:p:f", ["email=", "password=", "folder="])
+        argv = sys.argv[1:]
+
+        email = ''
+        password = ''
+        folder = 'inbox'
+        pattern = ''
+        limit = 1
+
+        opts, args = getopt.getopt(argv, "e:p:f:t:l", ["email=", "password=", "folder=", "pattern=", "limit="])
+
+        for opt, arg in opts:
+            if opt == '-h':
+                print 'cmd.py -e <email address> -p <password> [<folder> [<pattern> [<limit number>]]]'
+                sys.exit()
+            elif opt in ("-e", "--email"):
+                email = arg
+            elif opt in ("-p", "--password"):
+                password = arg
+            elif opt in ("-f", "--folder"):
+                folder = arg
+            elif opt in ("-t", "--pattern"):
+                pattern = arg
+            elif opt in ("-l", "--limit"):
+                limit = arg
+
+        mails = read_email_from_gmail(email, password, folder, pattern, limit)
+
+        print mails
     except getopt.GetoptError:
-        print 'cmd.py -e <email address> -p <password> [<folder>]'
+        print 'cmd.py -e <email address> -p <password> [<folder> [<pattern> [<limit number>]]'
         sys.exit(2)
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'cmd.py -e <email address> -p <password> [<folder>]'
-            sys.exit()
-        elif opt in ("-e", "--email"):
-            email = arg
-        elif opt in ("-p", "--password"):
-            password = arg
-        elif opt in ("-f", "--folder"):
-            folder = arg
-
-    read_email_from_gmail(email, password, folder)
--- lib/gmail.py
+++ lib/gmail.py
@@ -11,8 +11,16 @@ from config import SMTP_SERVER
 #
 # ------------------------------------------------
 
-def read_email_from_gmail(email_address, email_password, folder='inbox', extract_pattern='/(72 jam)/'):
+
+def read_email_from_gmail(email_address,
+                          email_password,
+                          folder='inbox',
+                          extract_pattern='',
+                          limit=0):
+    output_mails = []
     try:
+        limit = int(limit)
+        
         mail = imaplib.IMAP4_SSL(SMTP_SERVER)
         mail.login(email_address, email_password)
         mail.select(folder)
@@ -21,25 +29,29 @@ def read_email_from_gmail(email_address, email_password, folder='inbox', extract
         mail_ids = data[0]
 
         id_list = mail_ids.split()
-        first_email_id = int(id_list[0])
+        first_email_id = int(id_list[0]) if limit == 0 else limit
         latest_email_id = int(id_list[-1])
 
-        for i in range(latest_email_id,first_email_id, -1):
+        for i in range(latest_email_id, first_email_id, -1):
             typ, data = mail.fetch(i, '(RFC822)' )
 
             for response_part in data:
                 if isinstance(response_part, tuple):
                     msg = email.message_from_string(response_part[1])
                     body = clean_mail_body(msg)
-                    filtered_body = filter_text(body, extract_pattern)
-                    print filtered_body + "\n"
+                    filtered_body = filter_text(body, extract_pattern) if extract_pattern else body
+
+                    if filtered_body:
+                        output_mails.append(filtered_body)
 
     except Exception, e:
-        print str(e)
+        output_mails.append(str(e))
+
+    return output_mails
 
 def clean_mail_body(text):
     input_html = str(text)
-    soup = BeautifulSoup(input_html, "html.parser")
+    soup = BeautifulSoup(input_html, "lxml")
     output_text = soup.get_text()
     return output_text
 
